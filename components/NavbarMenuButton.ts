b9428fa6ff80de6128f9cb712e8f01eb2d2c2448
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-05 17:12:58 -0600

    feat: default layout (navbar/theme support)

---
+++ components/NavbarMenuButton.ts
@@ -0,0 +1,80 @@
+import Vue from 'vue'
+
+export default Vue.component('NavbarMenuButton', {
+  name: 'NavbarMenuButton',
+  functional: true,
+  props: {
+    isOpened: {
+      type: Boolean,
+      required: true
+    }
+  },
+  render(h, { props, listeners }) {
+    const line = {
+      fill: ' none',
+      transition: 'stroke-dasharray 500ms, stroke-dashoffset 500ms',
+      stroke: 'currentColor',
+      'stroke-width': '5.5',
+      'stroke-linecap': 'round'
+    }
+
+    const top = {
+      'stroke-dasharray': '40 139'
+    }
+    const bottom = {
+      'stroke-dasharray': '40 180'
+    }
+
+    return h(
+      'button',
+      {
+        on: listeners,
+        staticClass:
+          'flex items-center justify-center h-10 w-10 bg-gray-900 focus:outline-none rounded-full',
+        attrs: { type: 'button' }
+      },
+      [
+        h(
+          'svg',
+          {
+            attrs: { viewBox: '0 0 100 100' },
+            style: {
+              ...(props.isOpened ? { transform: 'rotate(45deg)' } : {})
+            },
+            staticClass:
+              'h-6 w-6 fill-current inline-block relative select-none',
+            staticStyle: {
+              transition: 'transform 400ms'
+            }
+          },
+          [
+            h('path', {
+              staticStyle: { ...line, ...top },
+              style: {
+                ...(props.isOpened ? { 'stroke-dashoffset': '-98px' } : {})
+              },
+              attrs: {
+                d:
+                  'm 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40'
+              }
+            }),
+            h('path', {
+              staticStyle: { ...line },
+              attrs: { d: 'm 30,50 h 40' }
+            }),
+            h('path', {
+              style: {
+                ...(props.isOpened ? { 'stroke-dashoffset': '-138px' } : {})
+              },
+              staticStyle: { ...line, ...bottom },
+              attrs: {
+                d:
+                  'm 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40'
+              }
+            })
+          ]
+        )
+      ]
+    )
+  }
+})
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,57 @@
 <template>
-  <div class="min-h-screen">
+  <div class="layout">
     <div class="flex items-stretch justify-between">
       <nuxt />
     </div>
+    <div
+      class="fixed flex items-center justify-around py-2 mx-auto w-11/12 navbar"
+      style="bottom: 1rem;"
+    >
+      <navbar-menu-button :is-opened="isMenuOpen" @click="theme" />
+
+      <nuxt-link to="/" class="font-medium text-lg">DEMO</nuxt-link>
+      <nuxt-link
+        to="/"
+        class="bg-gray-900 rounded-full h-10 w-10 flex-shrink-0 overflow-hidden flex items-center justify-center"
+      >
+        <span class="text-purple-400 text-xl font-medium">AT</span>
+      </nuxt-link>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { createComponent, ref, onBeforeMount } from '@vue/composition-api'
+import NavbarMenuButton from '~/components/NavbarMenuButton'
+
+export default createComponent({
+  components: { NavbarMenuButton },
+  setup() {
+    const isMenuOpen = ref(false)
+    const t = ref<string | null>('')
+
+    const theme = () => {
+      if (document.documentElement.classList.toggle('dark'))
+        localStorage.setItem('theme', 'dark')
+      else localStorage.removeItem('theme')
+    }
+
+    onBeforeMount(() => {
+      t.value = localStorage.getItem('theme')
+      if (t.value === 'dark') theme()
+    })
+
+    return { isMenuOpen, theme }
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+.layout {
+  @apply min-h-screen flex justify-center bg-light text-dark;
+}
+
+.navbar {
+  @apply bg-dark-highlight text-white;
+}
+</style>
