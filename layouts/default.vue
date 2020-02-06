39b5ebf3b875a601c46a6c2439048ab153697abd
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-06 17:29:09 -0600

    feat(navbar): separate navbar on SFC, update global layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,57 +1,21 @@
 <template>
-  <div class="layout">
+  <div class="min-h-screen flex justify-center bg-light text-dark">
     <div class="flex items-stretch justify-between">
       <nuxt />
     </div>
-    <div
-      class="fixed flex items-center justify-around py-2 mx-auto w-11/12 navbar"
-      style="bottom: 1rem;"
-    >
-      <navbar-menu-button :is-opened="isMenuOpen" @click="theme" />
 
-      <nuxt-link to="/" class="font-medium text-lg">DEMO</nuxt-link>
-      <nuxt-link
-        to="/"
-        class="bg-gray-900 rounded-full h-10 w-10 flex-shrink-0 overflow-hidden flex items-center justify-center"
-      >
-        <span class="text-purple-400 text-xl font-medium">AT</span>
-      </nuxt-link>
-    </div>
+    <nav-bar />
   </div>
 </template>
 
 <script lang="ts">
-import { createComponent, ref, onBeforeMount } from '@vue/composition-api'
-import NavbarMenuButton from '~/components/NavbarMenuButton'
+import { createComponent } from '@vue/composition-api'
+import NavBar from '~/components/NavBar.vue'
 
 export default createComponent({
-  components: { NavbarMenuButton },
+  components: { NavBar },
   setup() {
-    const isMenuOpen = ref(false)
-    const t = ref<string | null>('')
-
-    const theme = () => {
-      if (document.documentElement.classList.toggle('dark'))
-        localStorage.setItem('theme', 'dark')
-      else localStorage.removeItem('theme')
-    }
-
-    onBeforeMount(() => {
-      t.value = localStorage.getItem('theme')
-      if (t.value === 'dark') theme()
-    })
-
-    return { isMenuOpen, theme }
+    return {}
   }
 })
 </script>
-
-<style lang="postcss" scoped>
-.layout {
-  @apply min-h-screen flex justify-center bg-light text-dark;
-}
-
-.navbar {
-  @apply bg-dark-highlight text-white;
-}
-</style>
