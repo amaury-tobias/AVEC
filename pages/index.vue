9e30c24b301068651fc0c386fbac76455da69a88
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-07 16:25:45 -0600

    feat(index): add mockup on `index.vue`

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,23 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        e-front
-      </h1>
-      <h2 class="subtitle">
-        My gnarly Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="mb-20 flex justify-center items-center flex-grow flex-col">
+    <card-news
+      v-for="i in 10"
+      :key="i"
+      :image="i % 2 == 0 ? `https://picsum.photos/500?random=${i}` : undefined"
+      title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      date="20 Ene 2020"
+      description="Neiman Marcus, is an American Flagship department store which is
+              among the most prestigious luxury stores. However, its iOS App is
+              barely satisfactory."
+    />
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+import CardNews from '~/components/CardNews.vue'
 
-export default {
-  components: {
-    Logo
-  }
-}
+export default createComponent({
+  components: { CardNews }
+})
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
