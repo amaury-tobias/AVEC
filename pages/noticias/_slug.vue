4b75b65a319a2206f742a4489026337dd4dca0cb
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-12 17:29:17 -0600

    feat: add /noticias and /noticias/_slug routes

---
+++ pages/noticias/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <dynamic-markdown :name="slug" />
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+import DynamicMarkdown from '~/components/DynamicMarkdown.vue'
+
+export default createComponent({
+  components: { DynamicMarkdown },
+  asyncData({ params }) {
+    return {
+      slug: params.slug
+    }
+  }
+})
+</script>
---
+++ pages/noticias/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col justify-center items-center w-full p-2 md:p-0">
+    <card-news
+      v-for="i in 10"
+      :key="i"
+      :image="i % 2 == 0 ? `https://picsum.photos/500?random=${i}` : undefined"
+      title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      date="20 Ene 2020"
+      description="Neiman Marcus, is an American Flagship department store which is
+              among the most prestigious luxury stores. However, its iOS App is
+              barely satisfactory."
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+import CardNews from '~/components/CardNews.vue'
+
+export default createComponent({
+  components: { CardNews }
+})
+</script>
