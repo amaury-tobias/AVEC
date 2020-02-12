c33aebfbe40813a95956f025e189de7383e3e271
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-12 12:32:19 -0600

    feat: add `Sidebar.vue` component to use on lg: screens

---
+++ components/navigation/Sidebar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-dark-highlight h-screen sticky top-0">
+    <div class="flex flex-col items-start justify-start m-4 p-8 rounded-lg">
+      <a
+        class="rounded-full bg-dark-highlight py-1 px-3 my-2 flex items-baseline"
+        href=""
+      >
+        <fa-icon icon="home" />
+        <span class="font-normal text-xl">
+          Inicio
+        </span>
+      </a>
+      <a
+        class="rounded-full bg-dark-highlight py-1 px-3 my-2 felx items-baseline"
+        href=""
+      >
+        <fa-icon :icon="['far', 'user-circle']" />
+        <span class="font-normal text-xl">
+          Perfil
+        </span>
+      </a>
+      <a
+        class="rounded-full bg-dark-highlight py-1 px-3 my-2 flex items-baseline"
+        href=""
+      >
+        <fa-icon icon="cog" />
+        <span class="font-normal text-xl">
+          Ajustes
+        </span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({})
+</script>
