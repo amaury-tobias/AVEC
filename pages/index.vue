6703db5eebbaf1a0683b9619181eac1381b34fcc
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-13 17:55:29 -0600

    refactor: simplified index

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,3 @@
     Bienvenido
   </div>
 </template>
-
-<script lang="ts">
-import { createComponent } from '@vue/composition-api'
-import CardNews from '~/components/CardNews.vue'
-
-export default createComponent({
-  components: { CardNews }
-})
-</script>
