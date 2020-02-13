1a9f9501c1c79dfab68cce0b13c1574ab024e202
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-13 17:52:22 -0600

    feat: error component/default handler layout

---
+++ layouts/error.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="flex flex-col justify-center items-center font-light text-6xl text-center"
+  >
+    <p>{{ statusCode }}</p>
+    <p v-if="statusCode === 404" class="text-xl text-gray-600">
+      Lo sentimos pero esta pagina no fue encontrada
+      <br />
+      <nuxt-link class="underline" to="/">
+        ir al inicio
+      </nuxt-link>
+    </p>
+    <p v-else class="text-xl text-gray-600">
+      Ocurrio un error inesperado
+      <br />
+      <nuxt-link class="underline" to="/">
+        ir al inicio
+      </nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent, computed } from '@vue/composition-api'
+
+export default createComponent({
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  setup({ error }) {
+    const statusCode = computed(() => (error && error.statusCode) || 500)
+    return { statusCode }
+  }
+})
+</script>
