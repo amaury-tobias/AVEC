523a86d0d3305aa09783b7cc36433861da381a70
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-14 11:16:53 -0600

    feat(css): add color utils `gray-transparent` to darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
         light: 'var(--light-color)',
         dark: 'var(--dark-color)',
         'dark-highlight': 'var(--dark-highlight-color)',
-        'light-highlight': 'var(--light-highlight-color)'
+        'light-highlight': 'var(--light-highlight-color)',
+        'gray-transparent': 'var(--gray-transparent-color)'
       }
     }
   },
