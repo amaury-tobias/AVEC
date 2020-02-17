5843d67748d3c6045ba939475a615d1391aa3fbd
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-17 15:16:12 -0600

    feat: add `VAvatar` component

---
+++ components/VAvatar.vue
@@ -0,0 +1,38 @@
+<template functional>
+  <img
+    v-lazy="props.src"
+    class="shadow-xl rounded-full h-auto w-56 border-white border-4"
+    :alt="props.alt"
+  />
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({
+  props: {
+    src: {
+      type: String,
+      default: ''
+    },
+    alt: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+img {
+  @apply object-fill transition-opacity duration-1000;
+  &[lazy='loading'] {
+    @apply opacity-0;
+    filter: blur(150px);
+  }
+  &[lazy='loaded'] {
+    @apply opacity-100;
+  }
+  background: linear-gradient(45deg, #86fde8, #acb6e5);
+}
+</style>
