c9bca3492ee1dee1078f95880effeb3c31da57d5
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-18 11:47:12 -0600

    refactor: update usage of `CardNews` on `noticias/index`

--- pages/noticias/index.vue
+++ pages/noticias/index.vue
@@ -1,25 +1,26 @@
 <template>
-  <div
-    class="flex flex-col justify-center items-center h-full mt-4 mb-16 md:my-16 p-2 md:p-0"
-  >
-    <card-news
-      v-for="i in 10"
-      :key="i"
-      :image="i % 2 == 0 ? `https://picsum.photos/500?random=${i}` : undefined"
-      title="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      date="20 Ene 2020"
-      description="Neiman Marcus, is an American Flagship department store which is
-              among the most prestigious luxury stores. However, its iOS App is
-              barely satisfactory."
-    />
+  <div class="grid grid-cols-1 row-gap-4 mb-4 md:mb-0">
+    <card-news v-for="i in 10" :key="i" v-bind="cardNewsProps" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref } from '@vue/composition-api'
 import CardNews from '~/components/CardNews.vue'
 
 export default defineComponent({
-  components: { CardNews }
+  components: { CardNews },
+  setup() {
+    const cardNewsProps = ref({
+      title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      date: '20 Ene 2020',
+      image: `https://picsum.photos/500?random=${Math.random()}`,
+      description: `Neiman Marcus, is an American Flagship department store which is
+              among the most prestigious luxury stores. However, its iOS App is
+              barely satisfactory.`
+    })
+
+    return { cardNewsProps }
+  }
 })
 </script>
