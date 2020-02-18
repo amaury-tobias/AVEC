b1d620a4e04a73e4b27025b9e532b53629b20c1b
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2020-02-18 11:46:28 -0600

    refactor: `CardNews.vue` update style, props declaration, icons

--- components/CardNews.vue
+++ components/CardNews.vue
@@ -1,45 +1,29 @@
-<template functional>
+<template>
   <div
-    class="bg-dark-highlight rounded-lg shadow mb-4 border border-gray-transparent card-news"
+    class="bg-dark-highlight rounded-lg shadow border border-gray-transparent"
   >
     <nuxt-link to="/noticias/test" class="block no-underline">
       <img
-        v-if="props.image"
-        v-lazy="props.image"
+        v-if="image"
+        v-lazy="image"
         class="w-full h-64 object-cover rounded-t-lg"
         alt=""
       />
       <div class="px-6 py-6">
         <header>
           <h2 class="font-light text-2xl mb-2 capitalize">
-            {{ props.title }}
+            {{ title }}
           </h2>
-          <div class="text-xs flex items-center my-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="h-4 w-4 feather feather-calendar"
-            >
-              <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-              <line x1="16" y1="2" x2="16" y2="6"></line>
-              <line x1="8" y1="2" x2="8" y2="6"></line>
-              <line x1="3" y1="10" x2="21" y2="10"></line>
-            </svg>
+          <div class="text-xs flex items-center mb-2">
+            <calendar-icon size="14" />
             <span>
-              {{ props.date }}
+              {{ date }}
             </span>
           </div>
         </header>
         <article>
           <p class="leading-normal mb-4">
-            {{ props.description }}
+            {{ description }}
           </p>
         </article>
       </div>
@@ -48,26 +32,24 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, PropOptions } from '@vue/composition-api'
+import { CalendarIcon } from 'vue-feather-icons'
+
+const cardNewsProps = {
+  title: { required: true } as PropOptions<String, true>,
+  description: { required: true } as PropOptions<String, true>,
+  date: { required: true } as PropOptions<String, true>,
+  image: { required: true } as PropOptions<String, true>
+}
 
 export default defineComponent({
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    description: {
-      type: String,
-      required: true
-    },
-    date: {
-      type: String,
-      required: true
-    },
-    image: {
-      type: String,
-      required: false
-    }
-  }
+  components: { CalendarIcon },
+  props: { ...cardNewsProps }
 })
 </script>
+
+<style lang="postcss" scoped>
+img {
+  background: linear-gradient(45deg, var(--light-highlight-color), #43c6ac);
+}
+</style>
