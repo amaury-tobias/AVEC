2b4fba8d92e6d1929bac51b6d8aef9cd984da00b
amaury-tobias <amaury.tobiasqr@gmail.com>
2020-05-15 09:00:10 -0500

    style: lint `.eslintrc.js` on __client

--- __client/.eslintrc.js
+++ __client/.eslintrc.js
@@ -2,20 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:vue/recommended',
-    '@vue/prettier'
+    '@vue/prettier',
   ],
   plugins: [],
   // add your custom rules here
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
 }
